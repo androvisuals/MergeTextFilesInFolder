381d4a6317806d2ba3232dc3e5dbef4d03d063ed
AndroVisuals <androvisuals@gmail.com>
2017-09-18 17:25:19 +0200

    Quick cleanup

--- MergeTextFilesInFolder/MergeTextFilesInFolder/Program.cs
+++ MergeTextFilesInFolder/MergeTextFilesInFolder/Program.cs
@@ -19,26 +19,28 @@ namespace MergeTextFilesInFolder
             Console.WriteLine("Give a valid path to the folder.");
             string folderPath =   Console.ReadLine().ToString();
 
-            
-
+      
             string[] fileNamesArray = Directory.GetFiles(folderPath, "*" + fileTypeExtension);
             //test path here, to paste right click top of console and select/edit/paste
             // C:\Users\Andro\Desktop\Test Folder for VS project
 
-            if(fileNamesArray.Length == 0 )
+
+            if(fileNamesArray.Length == 0 )//if file type isn't present inform user
             {
                 Console.WriteLine("That file type is not present in the folder you selected");
             }
+            
             try
             {
+                string line = String.Empty;
                 
                 Console.WriteLine("You picked the file type " + fileTypeExtension);
                 Console.WriteLine("All the files that match your folder and selection are:");
                 //Display all files
                 foreach (string name in fileNamesArray)
                 {
-                    //writes each file name out for debugging purposes
-                    Console.WriteLine(Path.GetFileName( name));
+                   //writes each file name out for debugging purposes
+                   Console.WriteLine(Path.GetFileName( name));
                 }
             }
             catch (System.IO.DirectoryNotFoundException e )

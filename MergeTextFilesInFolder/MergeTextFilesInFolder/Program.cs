b014cd550160b5f31cd945b252d29bd4b8cec4e5
AndroVisuals <androvisuals@gmail.com>
2017-09-18 15:46:31 +0200

    Added read files names in Directory
    User gives a full folder path, all file names are printed out

--- MergeTextFilesInFolder/MergeTextFilesInFolder/Program.cs
+++ MergeTextFilesInFolder/MergeTextFilesInFolder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,27 @@ namespace MergeTextFilesInFolder
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Give a valid path to the folder.");
+            string folderPath =   Console.ReadLine().ToString();
+
+            //test path here
+            // C:\Users\Andro\Desktop\Test Folder for VS project
+            string[] fileNamesArray = Directory.GetFiles(folderPath) ;
+
+            //Display all files
+            foreach (string name in fileNamesArray)
+            {
+                //writes each file name out for debugging purposes
+                Console.WriteLine(name);
+            }
+            
+            Console.ReadLine();
         }
     }
+   
+
 }

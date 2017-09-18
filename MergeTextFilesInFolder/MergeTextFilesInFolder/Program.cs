bfd2bef587924cbcf85fdba892b1aed2b6815743
AndroVisuals <androvisuals@gmail.com>
2017-09-18 17:00:07 +0200

    Invalid file path is now caught, only file name now shown
    Invalid file path is now caught , informs the user that thefilename is invalid.
    Full path name of each file is no longer shown, only the actual file names and extension.

--- MergeTextFilesInFolder/MergeTextFilesInFolder/Program.cs
+++ MergeTextFilesInFolder/MergeTextFilesInFolder/Program.cs
@@ -13,28 +13,40 @@ namespace MergeTextFilesInFolder
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Define your file type to merge , example : .xml or .txt");
+            string fileTypeExtension = Console.ReadLine().ToString();
+
             Console.WriteLine("Give a valid path to the folder.");
             string folderPath =   Console.ReadLine().ToString();
 
-            Console.WriteLine("Define your file type, example : .xml or .txt");
-            string fileTypeExtension =  Console.ReadLine().ToString();
-
             
+
+            string[] fileNamesArray = Directory.GetFiles(folderPath, "*" + fileTypeExtension);
             //test path here, to paste right click top of console and select/edit/paste
             // C:\Users\Andro\Desktop\Test Folder for VS project
-            string[] fileNamesArray = Directory.GetFiles(folderPath, "*"+ fileTypeExtension) ;
-            if(fileNamesArray == null)
+
+            try
             {
-                fileNamesArray[0] = "That is not  valid file path";
+                
+                Console.WriteLine("You picked the file type " + fileTypeExtension);
+                Console.WriteLine("All the files that match your folder and selection are:");
+                //Display all files
+                foreach (string name in fileNamesArray)
+                {
+                    //writes each file name out for debugging purposes
+                    Console.WriteLine(Path.GetFileName( name));
+                }
             }
-            Console.WriteLine("You picked the file type " + fileTypeExtension);
-            Console.WriteLine("All the files that match your folder and selection are:");
-            //Display all files
-            foreach (string name in fileNamesArray)
+            catch (System.IO.DirectoryNotFoundException e )
             {
-                //writes each file name out for debugging purposes
-                Console.WriteLine(name);
+                //throw e;
+                Console.WriteLine("an error has occured: '{0}' ", e.Message);
             }
+
+                
+           
+
+            
             
             Console.ReadLine();
         }

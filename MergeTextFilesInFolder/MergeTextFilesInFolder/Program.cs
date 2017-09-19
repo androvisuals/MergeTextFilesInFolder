8b8a98f10f897e6482de24bf7a8e932f9f2f3752
AndroVisuals <androvisuals@gmail.com>
2017-09-19 13:58:34 +0200

    Basic merge working writes all to a new text file
    Basic merge working writes all text to a new text file with a  location defined by the user. Exception is thrown if the text file already exists.

--- MergeTextFilesInFolder/MergeTextFilesInFolder/Program.cs
+++ MergeTextFilesInFolder/MergeTextFilesInFolder/Program.cs
@@ -13,16 +13,23 @@ namespace MergeTextFilesInFolder
 
         static void Main(string[] args)
         {
+
+            
+            
             Console.WriteLine("Define your file type to merge , example : .xml or .txt");
             string fileTypeExtension = Console.ReadLine().ToString();
 
             Console.WriteLine("Give a valid path to the folder.");
             string folderPath =   Console.ReadLine().ToString();
 
-      
+            Console.WriteLine("Give a valid path for merged file");
+            string mergePath = Console.ReadLine().ToString();
+
+
             string[] fileNamesArray = Directory.GetFiles(folderPath, "*" + fileTypeExtension);
             //test path here, to paste right click top of console and select/edit/paste
-            // C:\Users\Andro\Desktop\Test Folder for VS project
+            // C:\Users\Andro\Desktop\Test Folder for VS project\
+            // C:\Users\Andro\Desktop\Test Folder for VS project\MergedFile.txt
 
 
             if(fileNamesArray.Length == 0 )//if file type isn't present inform user
@@ -32,15 +39,33 @@ namespace MergeTextFilesInFolder
             
             try
             {
-                string line = String.Empty;
-                
                 Console.WriteLine("You picked the file type " + fileTypeExtension);
                 Console.WriteLine("All the files that match your folder and selection are:");
                 //Display all files
                 foreach (string name in fileNamesArray)
                 {
-                   //writes each file name out for debugging purposes
-                   Console.WriteLine(Path.GetFileName( name));
+                    //writes each file name out for debugging purposes
+                    //Console.WriteLine(Path.GetFileName( name));
+
+
+                    string line = String.Empty;
+                    using (StreamReader reader = new StreamReader(name))
+                    {
+                        //reads each line
+                        line = reader.ReadToEnd();
+
+                        using (StreamWriter writer = new StreamWriter(mergePath))
+                        {
+                            
+
+                            writer.WriteLine(line);
+                        }
+
+                    }
+                    
+                    //Puts all text together
+                    Console.WriteLine(line);
+                    
                 }
             }
             catch (System.IO.DirectoryNotFoundException e )
@@ -49,11 +74,11 @@ namespace MergeTextFilesInFolder
                 Console.WriteLine("an error has occured: '{0}' ", e.Message);
             }
 
-                
-           
+            
 
-            
-            
+
+
+
             Console.ReadLine();
         }
     }

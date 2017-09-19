e9bf43e4621e5df326ce2c997bdf5dbe43b03509
AndroVisuals <androvisuals@gmail.com>
2017-09-19 14:25:45 +0200

    Merged file cleanup and moved most of the code into functions
    Trying to create a new merged file causes an exception to be thrown as the file is still in use. Deleting the file on startup solves the problem. Uses the same path as the merged file location.

--- MergeTextFilesInFolder/MergeTextFilesInFolder/Program.cs
+++ MergeTextFilesInFolder/MergeTextFilesInFolder/Program.cs
@@ -9,34 +9,28 @@ namespace MergeTextFilesInFolder
 {
     class Program
     {
-        
+
 
         static void Main(string[] args)
         {
 
-            
-            
-            Console.WriteLine("Define your file type to merge , example : .xml or .txt");
-            string fileTypeExtension = Console.ReadLine().ToString();
-
-            Console.WriteLine("Give a valid path to the folder.");
-            string folderPath =   Console.ReadLine().ToString();
-
-            Console.WriteLine("Give a valid path for merged file");
-            string mergePath = Console.ReadLine().ToString();
 
 
-            string[] fileNamesArray = Directory.GetFiles(folderPath, "*" + fileTypeExtension);
+            string fileTypeExtension = FileTypeToMerge();
+            string folderPath = FolderPathWithFilesToMerge();
+            string mergePath = MergeFilesPath();
+            //deletes any previous merged file to avoid exceptions being thrown
+            DeleteMergedFile(mergePath);
+
+            string[] fileNamesArray = FileNamesToArray(folderPath,fileTypeExtension);
+            //string[] fileNamesArray = Directory.GetFiles(folderPath, "*" + fileTypeExtension);
             //test path here, to paste right click top of console and select/edit/paste
             // C:\Users\Andro\Desktop\Test Folder for VS project\
             // C:\Users\Andro\Desktop\Test Folder for VS project\MergedFile.txt
 
 
-            if(fileNamesArray.Length == 0 )//if file type isn't present inform user
-            {
-                Console.WriteLine("That file type is not present in the folder you selected");
-            }
             
+
             try
             {
                 Console.WriteLine("You picked the file type " + fileTypeExtension);
@@ -56,31 +50,59 @@ namespace MergeTextFilesInFolder
 
                         using (StreamWriter writer = new StreamWriter(mergePath))
                         {
-                            
-
                             writer.WriteLine(line);
                         }
-
                     }
-                    
                     //Puts all text together
                     Console.WriteLine(line);
-                    
                 }
             }
-            catch (System.IO.DirectoryNotFoundException e )
+            catch (System.IO.DirectoryNotFoundException e)
             {
                 //throw e;
                 Console.WriteLine("an error has occured: '{0}' ", e.Message);
             }
 
-            
-
-
-
-
             Console.ReadLine();
         }
+        //request user to type in what file type they want to merge
+        static string FileTypeToMerge()
+        {
+
+            Console.WriteLine("Define your file type to merge , example : .xml or .txt");
+            string fileTypeExtension = Console.ReadLine().ToString();
+            return fileTypeExtension;
+        }
+        //request user to type in the folder path where the files are to merge
+        static string FolderPathWithFilesToMerge()
+        {
+            //Console.WriteLine("Give a valid path to the folder.");
+            //return Console.ReadLine().ToString();
+            return @"C:\Users\Andro\Desktop\Test Folder for VS project\";
+        }
+        static void DeleteMergedFile(string deletePath)
+        {
+            File.Delete(deletePath);
+            
+        }
+        static string MergeFilesPath()
+        {
+            //Console.WriteLine("Give a valid path for merged file");
+            //return Console.ReadLine().ToString();
+            return @"C:\Users\Andro\Desktop\Test Folder for VS project\MergedFile.txt";
+        }
+        static string[] FileNamesToArray(string folderPath, string fileTypeExtension)
+        {
+            string[] fileNamesArray = Directory.GetFiles(folderPath, "*" + fileTypeExtension);
+            if (fileNamesArray.Length == 0)//if file type isn't present inform user
+            {
+                Console.WriteLine("That file type is not present in the folder you selected");
+            }
+            return fileNamesArray;
+        }
+
+      
+
     }
    
 

0f6ca7e924152225af25db3a042de817b2a103f4
AndroVisuals <androvisuals@gmail.com>
2017-09-19 16:27:54 +0200

    Now works with standard text files
    Now works with standard text files, keeps text formatting as well. Rtf format still isn't working as expected.

--- MergeTextFilesInFolder/MergeTextFilesInFolder/Program.cs
+++ MergeTextFilesInFolder/MergeTextFilesInFolder/Program.cs
@@ -18,79 +18,100 @@ namespace MergeTextFilesInFolder
 
             string fileTypeExtension = FileTypeToMerge();
             string folderPath = FolderPathWithFilesToMerge();
-            string mergePath = MergeFilesPath();
+            string mergePath = MergeFilesPath("MergedFiles"+fileTypeExtension);
+
             //deletes any previous merged file to avoid exceptions being thrown
             DeleteMergedFile(mergePath);
 
             string[] fileNamesArray = FileNamesToArray(folderPath,fileTypeExtension);
             //string[] fileNamesArray = Directory.GetFiles(folderPath, "*" + fileTypeExtension);
-            //test path here, to paste right click top of console and select/edit/paste
-            // C:\Users\Andro\Desktop\Test Folder for VS project\
-            // C:\Users\Andro\Desktop\Test Folder for VS project\MergedFile.txt
-
-
-            
-
+           
             try
             {
                 Console.WriteLine("You picked the file type " + fileTypeExtension);
                 Console.WriteLine("All the files that match your folder and selection are:");
                 //Display all files
-                foreach (string name in fileNamesArray)
+                
+                
+                int iteration = 0;
+                string finalResult = String.Empty;
+
+            //Console.WriteLine("End of line !");
+            
+
+
+            foreach (string name in fileNamesArray)
                 {
                     //writes each file name out for debugging purposes
-                    //Console.WriteLine(Path.GetFileName( name));
+                    //Console.WriteLine("The file name is " + Path.GetFileName( name));
+                    iteration += 1;
+                    Console.WriteLine(" this is iteration" + iteration);
+                    Console.WriteLine(" the name variable is " + name);
 
 
-                    string line = String.Empty;
                     using (StreamReader reader = new StreamReader(name))
                     {
                         //reads each line
-                        line = reader.ReadToEnd();
+                        string line = reader.ReadToEnd();
+                        
+                        finalResult += line;
+                        Console.WriteLine(" iteration inside Streamreader is " + iteration);
 
-                        using (StreamWriter writer = new StreamWriter(mergePath))
-                        {
-                            writer.WriteLine(line);
-                        }
+                        using (StreamWriter writer = File.AppendText(mergePath))// new StreamWriter(mergePath))
+                            {
+                                writer.Write(finalResult);
+                                // writer.Write("This is a test to write into text file , iteration is "+ iteration );
+                            }
                     }
-                    //Puts all text together
-                    Console.WriteLine(line);
+                     
+                //Console.WriteLine("Line writer is " + line);
+                //Puts all text together
+                //Console.WriteLine(line);
                 }
             }
+            
             catch (System.IO.DirectoryNotFoundException e)
             {
                 //throw e;
                 Console.WriteLine("an error has occured: '{0}' ", e.Message);
             }
+            /*
+            catch ()
+            {
 
+            }*/
             Console.ReadLine();
         }
         //request user to type in what file type they want to merge
         static string FileTypeToMerge()
         {
-
             Console.WriteLine("Define your file type to merge , example : .xml or .txt");
             string fileTypeExtension = Console.ReadLine().ToString();
             return fileTypeExtension;
         }
+
         //request user to type in the folder path where the files are to merge
         static string FolderPathWithFilesToMerge()
         {
-            //Console.WriteLine("Give a valid path to the folder.");
-            //return Console.ReadLine().ToString();
-            return @"C:\Users\Andro\Desktop\Test Folder for VS project\";
+            Console.WriteLine("Give a valid path to the folder.");
+            return Console.ReadLine().ToString();
+            //return @"C:\Users\Andro\Desktop\Test Folder for VS project\";
         }
+
         static void DeleteMergedFile(string deletePath)
         {
             File.Delete(deletePath);
             
         }
-        static string MergeFilesPath()
+
+        static string MergeFilesPath(string fileExtensionType)
         {
-            //Console.WriteLine("Give a valid path for merged file");
-            //return Console.ReadLine().ToString();
-            return @"C:\Users\Andro\Desktop\Test Folder for VS project\MergedFile.rtf";
+            Console.WriteLine("Give a valid path for merged file");
+            string userInput =  Console.ReadLine().ToString();
+            return userInput+@"\"+fileExtensionType;
+            //return @"C:\Users\Andro\Desktop\Test Folder for VS project\MergedFile.rtf";
         }
+
         static string[] FileNamesToArray(string folderPath, string fileTypeExtension)
         {
             string[] fileNamesArray = Directory.GetFiles(folderPath, "*" + fileTypeExtension);

c746bb404cade86bfec3f9f5d4ffd4229986bdcd
AndroVisuals <androvisuals@gmail.com>
2017-09-19 19:30:04 +0200

    GUI Implementation Folder browse dialog added
    Started GUI Implementation.
    Folder browse dialog added, returns sysem path as string.

---
+++ MergeTextFilesinFolderGUI/MergeTextFilesinFolderGUI/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WinForms = System.Windows.Forms;
+
+namespace MergeTextFilesinFolderGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectFolderClick(object sender, RoutedEventArgs e)
+        {
+            WinForms.FolderBrowserDialog folderDialog = new WinForms.FolderBrowserDialog();
+            folderDialog.ShowNewFolderButton = false;
+            folderDialog.SelectedPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            WinForms.DialogResult result = folderDialog.ShowDialog();
+
+            if (result == WinForms.DialogResult.OK)
+            {
+                //----< Selected Folder >---- 
+                //< Selected Path > 
+                String sPath = folderDialog.SelectedPath;
+                //tbxFolder.Text = sPath;
+                //</ Selected Path >
+                Console.WriteLine("Select Folder Button has been clicked");
+                Console.WriteLine("The folder path you selected is " + sPath);
+            }
+        }
+    }
+}
